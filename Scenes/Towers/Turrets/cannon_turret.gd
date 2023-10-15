1f9c2c7424d86ae0fcd420855603b3ecafadfc7c
Shadoscythe <cadendoo22@gmail.com>
2023-10-15 02:06:06 -0600

    Added Gatling and Laser turret

--- Scenes/Towers/Turrets/cannon_turret.gd
+++ Scenes/Towers/Turrets/cannon_turret.gd
@@ -4,9 +4,9 @@ var target
 
 func _ready():
 	target = $"../SceneElements/Path3D/TestTarget/AimPoint"
-
+	
 func _physics_process(delta):
 	turret_aim()
-
+	
 func turret_aim():
-	get_node("TurretHead").look_at(to_global(target.to_global(position)), Vector3(0,1,0), true) ## looks at target position, using the default up vector, and using the model's own "forward" direction
+	$TurretHead.look_at(target.global_position, Vector3(0,1,0), true) ## looks at target position, using the default up vector, and using the model's own "forward" direction)
