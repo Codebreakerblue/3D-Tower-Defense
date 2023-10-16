9b2f2692d3502e63ddcf27c54070c5325e736bc0
Shadoscythe <cadendoo22@gmail.com>
2023-10-15 21:10:50 -0600

    Added test map

--- Scenes/Towers/Turrets/CannonTurret/turret.gd
+++ Scenes/Towers/Turrets/CannonTurret/turret.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var target = $/root/GameScene/SceneElements/TestTargetPath/TestTarget/AimPoint
+@onready var target = $"../TestMap/EnemyPath/TestEnemy"
 @onready var aim_component = $TurretHead/AimLerp3DComponent
 
 func _ready():
