dd8fddfb312c6d0b70a7984dc66d6f39065656eb
Shadoscythe <cadendoo22@gmail.com>
2023-10-17 20:25:29 -0600

    Projectiles now travel in a straight line

--- Resources/Components/fire_component.gd
+++ Resources/Components/fire_component.gd
@@ -1,13 +1,16 @@
 extends Node
 
-var target
+var target #might be useless now
 
 var ready_to_fire = true
 var rof = .1
 
-@export var projectile_scene_1 : PackedScene
-@export var target_provider : Node
-@export var projectile_life = 1
+#Set using inspector
+@export var projectile_scene_1 : PackedScene	#what projectile to shoot
+@export var target_provider : Node	#what node is sending target information
+@export var fire_point : Node	#what point to spawn projectiles on
+@export var projectile_life = 1 #how long in seconds projectiles last before despawning
+
 
 func _physics_process(delta):
 	fire()
@@ -22,12 +25,12 @@ func fire():
 
 
 func spawn_projectile(projectile_type):
-	var projectile = projectile_scene_1.instantiate()
+	var projectile = projectile_type.instantiate()
 	add_child(projectile)
-	projectile.position = get_parent().global_position
-	projectile.look_at(target.position)
+	projectile.position = fire_point.global_position
+	projectile.look_at(target.global_position)
 	await get_tree().create_timer(1).timeout
 	projectile.queue_free()
 
-func assign_aimed_target():
+func assign_aimed_target():	##might be useless now
 	target = target_provider.target
--- Scenes/Towers/Turrets/projectile_component.gd
+++ Scenes/Towers/Turrets/projectile_component.gd
@@ -1,12 +1,17 @@
 extends Node
 
-@onready var parent = get_parent()
-@onready var aimer = get_parent().get_parent().get_parent()
+@onready var parent = get_parent()	#gets parent I.E. the root of the projectile
+@onready var aimer = get_parent().get_parent().get_parent() #gets the node
 @export var speed = 10
 
+
+func _ready():
+#	var forward = aimer.transform.basis.z
+#	projectile.look_at(target.position)
+	pass
 func move(delta):
-	var velocity = aimer.transform.basis.z
-	parent.position += velocity * speed * delta
+	var forward = -parent.transform.basis.z
+	parent.position += forward * speed * delta
 
 
 func _physics_process(delta):
